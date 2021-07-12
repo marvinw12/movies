51d21b79c01f0c46a79757f18eaf3e522ae9fe44
Marvin W <marvwood12@gmail.com>
2021-07-11 20:18:12 -0400

    creating URL

--- projects/secretmsg/index.js
+++ projects/secretmsg/index.js
@@ -1,9 +1,11 @@
-document.querySelector('form').addEventListener('submit', event => {
-    event.preventDefault();
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    const input = document.querySelector('#message-input');
+  const input = document.querySelector("#message-input");
 
-    const encrypted = btoa(input.value);
+  const encrypted = btoa(input.value);
 
-    document.querySelector('#link-input').value = encrypted;
-})
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
